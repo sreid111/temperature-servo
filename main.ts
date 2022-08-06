a412ff432f25ba12c7ab652432ad74d690637b37
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-08-06 10:54:29 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let temp = 0
+basic.showString("Servo temp")
 basic.forever(function () {
-    temp = 50
+    temp = input.temperature()
     basic.showNumber(temp)
     temp = Math.map(temp, 0, 40, 0, 180)
     servos.P0.setAngle(temp)
